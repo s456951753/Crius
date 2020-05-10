ebbc43662ddb9e2a24d0fd09d5df749209c4670c
Yuan Liu <ee07b238@gmail.com>
2020-05-10 22:43:58 +1200

    Ignore local properties file. (#4)

--- Test/Unit_test/Utils_test/configuration_file_service_test.py
+++ Test/Unit_test/Utils_test/configuration_file_service_test.py
@@ -4,10 +4,9 @@ import Utils.configuration_file_service as config_service
 
 class MyTestCase(unittest.TestCase):
     def test_getProperty(self):
-        self.assertEqual(
+        self.assertIsNotNone(
             config_service.getProperty(section_name=config_service.TOKEN_SECTION_NAME,
-                                       property_name=config_service.TS_TOKEN_NAME),
-            "0123456789abcdeFG"
+                                       property_name=config_service.TS_TOKEN_NAME)
         )
 
 
