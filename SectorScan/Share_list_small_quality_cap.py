e47f44b1c770185be5215bae4460d0a894b22f44
Austin <austinyxh@hotmail.com>
2020-05-10 22:52:52 +1000

    AY update

--- SectorScan/Share_list_small_quality_cap.py
+++ SectorScan/Share_list_small_quality_cap.py
@@ -1,5 +1,5 @@
-import datetime
-
+from datetime import datetime, timedelta
+import time as t
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -9,58 +9,62 @@ token='3c311a0c0eb056bfe6c27a161e6cab275649b74245cfd5679a75dca9'
 pro=ts.pro_api(token)
 
 #Setup - fundamental section
-snapshot_date = 20200507
+snapshot_date = '20200507'
 small_cap_cutoff_up = 1150000 #defind the cutoff for small caps upper end （unit=万元）Note the cutoff is consistent with MSCI small cap definition
 small_cap_cutoff_low = 1050000 #defind the cutoff for small caps lower end （unit=万元）
 pe_cutoff_up = 30 #defind the cutoff for stock PE ratio
 
 #Setup - technical section
 
+
 #Extract basic stock information
 #sector_full_list = pro.query('stock_basic', exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,is_hs')
 
-#Extract basic stock information - company introduciton
+
+#Extract basic stock information - company introduction
 #sector_full_list_intro = pro.stock_company(exchange='SZSE', fields='ts_code,chairman,manager,secretary,website,introduction,main_business')
 
-#Extract basic stock information - trading snapshot
+#get daily metrics
 sector_full_list_snapshot = pro.query('daily_basic', ts_code='', trade_date=snapshot_date,fields='ts_code,turnover_rate_f,volume_ratio,pe_ttm,dv_ratio,free_share,total_mv')
-
-stocks = pro.query('daily_basic', ts_code='', trade_date=snapshot_date,fields='ts_code,list_date')
-stocks = stocks[(snapshot_date.date() - stocks.start_date) > datetime.timedelta(30)]
-
-for stock in stock_list:
-    days_public=(context.current_dt.date() - get_security_info(stock).start_date).days
-    if days_public<30:
-    stock_list.remove(stock)
 
 #Select small cap stocks with pre-defined cutoffs and with pre-defined PE range
 templist1 = sector_full_list_snapshot[sector_full_list_snapshot['total_mv'].between(small_cap_cutoff_low, small_cap_cutoff_up) & sector_full_list_snapshot['pe_ttm'].between(0.01, pe_cutoff_up)]
 
 
-#remove stocks that had up limted in the last x days
+#remove stocks that had up limited in the last x days
 up_start_date = 20200424 #TODO: this part to be automated later, reference snapshot_date
 up_end_date = 20200507 #TODO: this part to be automated later reference snapshot_date
 
 up_list = pro.limit_list(start_date=up_start_date, end_date=up_end_date)['ts_code']
 
-#select stocks with pre-defined PE range
-templist2 = templist1.loc[~(templist1.ts_code.isin(up_list['ts_code'])]
+templist2 = templist1[~templist1.ts_code.isin(up_list)]
+t.sleep(1)
 
-templist3 = templist2['ts_code'].to_list()
+#filter on stocks with at least three years trading history #TODO: the three year variable shoudl be an input field
+list_days_filter = pro.query('stock_basic', exchange='', list_status='L', fields='ts_code,list_date')
+list_days_filter['list_date'] = pd.to_datetime(list_days_filter['list_date'],format='%Y%m%d')
+list_days_filter['snapshot_date'] = snapshot_date
+list_days_filter['snapshot_date'] = pd.to_datetime(list_days_filter['snapshot_date'],format='%Y%m%d')
+list_days_filter['list_days'] = (list_days_filter['snapshot_date'] - list_days_filter['list_date']).dt.days
+list_days_filter2 = list_days_filter[list_days_filter['list_days'] > 730]
+
+templist3 = templist2[~templist2.ts_code.isin(list_days_filter2)]
+
+print(templist3)
 
 #get multi-year key financial info
-fina_start_date = 20190930 #TODO: this part to be automated later, reference snapshot_date
-fina_end_date = 20191231 #TODO: this part to be automated later reference snapshot_date
+#fina_start_date = 20190930 #TODO: this part to be automated later, reference snapshot_date
+#fina_end_date = 20191231 #TODO: this part to be automated later reference snapshot_date
 
-fin_data = {}
-for ticker in templist3:
-    fin_data = pro.query('fina_indicator_vip', ts_code=ticker, start_date=fina_start_date, end_date=fina_end_date, fields='ts_code,end_date,debt_to_eqt,roe_avg,gross_margin,ebt_yoy')
+#fin_data = {}
+#for ticker in templist3:
+#    fin_data = pro.query('fina_indicator_vip', ts_code=ticker, start_date=fina_start_date, end_date=fina_end_date, fields='ts_code,end_date,debt_to_eqt,roe_avg,gross_margin,ebt_yoy')
 
 #Strategy one
 #Look for mid to small cap, reasonable PE, no up limited in the last 30 days, reasonable gearing,
 
-#print(templist1)
-print(templist2)
+
+
 
 
 #Export the df to excel
--- SectorScan/temp place.py
+++ SectorScan/temp place.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta
 
 import pandas as pd
 import seaborn as sns
@@ -8,17 +8,21 @@ import tushare as ts
 token='3c311a0c0eb056bfe6c27a161e6cab275649b74245cfd5679a75dca9'
 pro=ts.pro_api(token)
 
-snapshot_date = 20200507
+snapshot_date = '20200507'
 
-stocks1 = pro.query('stock_basic', ts_code='600634.SH，000835.SZ', trade_date=snapshot_date,fields='ts_code,list_date')
+list_days_filter = pro.query('stock_basic', exchange='', list_status='L', fields='ts_code,list_date')
+list_days_filter['list_date'] = pd.to_datetime(list_days_filter['list_date'],format='%Y%m%d')
+list_days_filter['snapshot_date'] = snapshot_date
+list_days_filter['snapshot_date'] = pd.to_datetime(list_days_filter['snapshot_date'],format='%Y%m%d')
+list_days_filter['list_days'] = (list_days_filter['snapshot_date'] - list_days_filter['list_date']).dt.days
+list_days_filter2 = list_days_filter[list_days_filter['list_days'] > 730]
 
-print(stocks1)
+list_days_filter_list = list_days_filter2['ts_code'].tolist()
 
 
-ticker_list = stocks1['ts_code'].to_list()
+#Export the df to excel
+#list_days_filter.to_excel(r'C:\Users\Austin\Desktop\Tushare\list3.xlsx', index = False)
 
-for ticker in ticker_list:
-    days_public=pd.to_datetime(snapshot_date,format='%Y%m%d') - pd.to_datetime(stocks1['list_date'],format='%Y%m%d')
-if days_public<30:
-    stocks1.remove(ticker)
+print(list_days_filter_list)
 
+#df1['snapshot_date'] = datetime.strptime(snapshot_date, '%Y%m%d').date()
