982ce1c755ff850deac39719aa97e5dbf9eb6028
Yuan Liu <ee07b238@gmail.com>
2020-05-10 23:05:15 +1200

    Develop/implement tushare datasrc for rqalpha (#5)
    * 1. modify gitignore that I forgot to update
    2. add tushare datasrc for rqalpha
    
    * Ignore local properties file.

--- SectorScan/Share_list_small_quality_cap.py
+++ SectorScan/Share_list_small_quality_cap.py
@@ -4,17 +4,20 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+import Utils.configuration_file_service as config_service
 import tushare as ts
-token='3c311a0c0eb056bfe6c27a161e6cab275649b74245cfd5679a75dca9'
-pro=ts.pro_api(token)
 
-#Setup - fundamental section
+token = config_service.getProperty(section_name=config_service.TOKEN_SECTION_NAME,
+                                   property_name=config_service.TS_TOKEN_NAME)
+pro = ts.pro_api(token)
+
+# Setup - fundamental section
 snapshot_date = 20200507
-small_cap_cutoff_up = 1150000 #defind the cutoff for small caps upper end （unit=万元）Note the cutoff is consistent with MSCI small cap definition
-small_cap_cutoff_low = 1050000 #defind the cutoff for small caps lower end （unit=万元）
-pe_cutoff_up = 30 #defind the cutoff for stock PE ratio
+small_cap_cutoff_up = 1150000  # defind the cutoff for small caps upper end （unit=万元）Note the cutoff is consistent with MSCI small cap definition
+small_cap_cutoff_low = 1050000  # defind the cutoff for small caps lower end （unit=万元）
+pe_cutoff_up = 30  # defind the cutoff for stock PE ratio
 
-#Setup - technical section
+# Setup - technical section
 
 #Extract basic stock information
 #sector_full_list = pro.query('stock_basic', exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,is_hs')
