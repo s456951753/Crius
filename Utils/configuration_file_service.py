ad616f484410b480bef4e0ce1f9f04fa8488df7a
Milo Liu <milo.liu@rhapsody.health>
2020-05-11 13:58:54 +1200

    ?Not sure why properties file service not updated

--- Utils/configuration_file_service.py
+++ Utils/configuration_file_service.py
@@ -4,7 +4,8 @@ TOKEN_SECTION_NAME = "TOKENS"
 TS_TOKEN_NAME = "ts_token"
 
 config = ConfigParser()
-config.read("crius.properties")
+config.read("../crius.properties")
+
 
 def getProperty(section_name, property_name):
     return config[section_name][property_name]
