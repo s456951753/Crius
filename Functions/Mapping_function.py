26befdeed3362b9b069d044e9c912f67c6ecd66b
9527 <ee07b238@gmail.com>
2020-05-15 10:25:21 +1200

    Convert symbols from tusharea to alpha or vise versa. And its unit test

--- Functions/Mapping_function.py
+++
@@ -1,6 +0,0 @@
-#TODO: #请define 一个新功能来转化Tushare 和 Rqalpha 的股票代码。 一个功能是 Tushare 转 Rqalpha 另一个是 Rqalpha 转 Tushare
-
-#TODO:Tushare 的股票代码格式 举例： 000690.SZ （深市）or 603328.SH （沪市）
-
-#TODO:Rqalpha 的股票代码格式 举例： 000690.XSHE （深市）or 603328.XSHG （沪市）
-
