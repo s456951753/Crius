fbef9a0d533390c8094550b7325eb7fd8e787570
Yuan Liu <ee07b238@gmail.com>
2020-05-15 13:48:51 +1200

    Implement calculation of closest half year. Refactored a little bit of symbol conversion

--- Test/Unit_test/Utils_test/numeric_utils_test.py
+++ Test/Unit_test/Utils_test/numeric_utils_test.py
@@ -1,18 +1,25 @@
 import unittest
 import Utils.numeric_utils as nu
+import datetime
 
 
 class MyTestCase(unittest.TestCase):
-    def test_convert_successful(self):
+    def test_convert_stock_symbol_successful(self):
         self.assertEqual(nu.get_converted_stock_code('000690.SZ'), '000690.XSHE')
         self.assertEqual(nu.get_converted_stock_code('603328.SH'), '603328.XSHG')
         self.assertEqual(nu.get_converted_stock_code('000690.XSHE'), '000690.SZ')
         self.assertEqual(nu.get_converted_stock_code('603328.XSHG'), '603328.SH')
 
-    def test_validation_failed(self):
-        with self.assertRaises(nu.NameInvalidException):
+    def test_stock_symbol_validation_failed(self):
+        with self.assertRaises(nu.SymbolInvalidException):
             nu.get_converted_stock_code("asd9zCv&*()ds/.")
 
+    def test_get_closest_half_year_successful(self):
+        self.assertEqual(nu.get_closest_half_year(), "20191231")
+        self.assertEqual(nu.get_closest_half_year("20000630"), "20000630")
+        self.assertEqual(nu.get_closest_half_year("19751231"), "19751231")
+        self.assertEqual(nu.get_closest_half_year("20200101"), "20191231")
+        self.assertEqual(nu.get_closest_half_year("30220709"), "30220630")
 
 if __name__ == '__main__':
     unittest.main()
--- Utils/numeric_utils.py
+++ Utils/numeric_utils.py
@@ -1,13 +1,17 @@
 # DONE: #请define 一个新功能来转化Tushare 和 Rqalpha 的股票代码。 一个功能是 Tushare 转 Rqalpha 另一个是 Rqalpha 转 Tushare
-
 # DONE:Tushare 的股票代码格式 举例： 000690.SZ （深市）or 603328.SH （沪市）
-
 # DONE:Rqalpha 的股票代码格式 举例： 000690.XSHE （深市）or 603328.XSHG （沪市）
 
 import re
+import dateutil
+import datetime
 
 
 def get_converted_stock_code(symbol):
+    """
+    Based on the symbol given, convert symbol from rqalpha to tushare or vise versa. Will check if the symbol meets format
+    criteria, i.e. ######.SZ/XSHE/SH/XSHG and throw SymbolInvalidException if didn't meet
+    """
     validate(symbol)
     symbol = str(symbol)
     new_symbol = symbol.split('.')
@@ -18,13 +22,48 @@ def get_converted_stock_code(symbol):
     return new_symbol[0] + '.' + dic.get(new_symbol[1])
 
 
+def get_closest_half_year(snapshot_date=datetime.datetime.strftime(datetime.date.today(), "%Y%m%d")):
+    """
+    find the closest end day of June/December, depends on which one is closer to the given date.
+    :raise ValueError if format is invalid
+    :param snapshot_date:a date string in format %Y%m%d. If left empty, today's date will be used.
+    :return: a string object %Y%m%d, representing the closest end day of June/December, depends on which one is closer to the given date
+    """
+    try:
+        datetime.datetime.strptime(snapshot_date, '%Y%m%d')
+    except ValueError:
+        raise ValueError("Incorrect data format, should be YYYYMMDD")
+    new_date = datetime.date.fromisoformat(snapshot_date[0:4] + "-" + snapshot_date[4:6] + "-" + snapshot_date[6:])
+    new_year = new_date.year
+    new_month = new_date.month
+    new_day = new_date.day
+    if (new_month < 6):
+        new_month = 12
+        new_day = 31
+        new_year = new_year - 1
+    elif (new_month > 6 and new_month < 12):
+        new_month = 6
+        new_day = 30
+    elif (new_month == 6):
+        if (new_day != 30):
+            new_month = 12
+            new_day = 31
+            new_year = new_year - 1
+    elif (new_month == 12):
+        if (new_day != 31):
+            new_month = 6
+            new_day = 30
+
+    return datetime.datetime.strftime(datetime.date(new_year, new_month, new_day), "%Y%m%d")
+
+
 def validate(symbol):
     a = re.compile("\d{6}\.SZ|SH|XSHE|XSHG")
     if (a.search(symbol) == None):
-        raise NameInvalidException(symbol)
+        raise SymbolInvalidException(symbol)
 
 
-class NameInvalidException(Exception):
+class SymbolInvalidException(Exception):
     def __init__(self, symbol):
         self.symbol = symbol
         self.message = symbol + " is not a valid symbol"
