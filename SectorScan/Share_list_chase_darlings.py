75213d276787bceaba084a519bcefee78209eeca
Austin <austinyxh@hotmail.com>
2020-06-01 16:00:19 +1000

    AY

--- SectorScan/Share_list_chase_darlings.py
+++ SectorScan/Share_list_chase_darlings.py
@@ -88,5 +88,36 @@ fund_list_snapshot_prior2 = pd.pivot_table(fund_list_snapshot_prior2,index=["sym
 fund_list_snapshot_prior2 = fund_list_snapshot_prior2.sort_values(by='stk_float_ratio', ascending=False)
 
 #Export the df to excel
-fund_list_snapshot2.to_excel(r'C:\Users\Austin\Desktop\Tushare\list3.xlsx', index = True)
-fund_list_snapshot_prior2.to_excel(r'C:\Users\Austin\Desktop\Tushare\list2.xlsx', index = True)
+fund_list_snapshot2.to_excel(r'C:\Users\Austin\Desktop\Tushare\fund_list_snapshot.xlsx', index = True)
+fund_list_snapshot_prior2.to_excel(r'C:\Users\Austin\Desktop\Tushare\fund_list_snapshot_prior.xlsx', index = True)
+
+#本季度和上一季度基金公司持股文件读取
+fund_list = pd.read_excel (r'C:\Users\Austin\Desktop\Tushare\fund_list_snapshot.xlsx', index = False)
+fund_list_prior = pd.read_excel (r'C:\Users\Austin\Desktop\Tushare\fund_list_snapshot_prior.xlsx', index = False)
+
+#上季度列名称改名，防止重复
+fund_list_prior = fund_list_prior.rename(columns={'amount': 'amount_p', 'mkv': 'mkv_p', 'stk_float_ratio': 'stk_float_ratio_p', 'ts_code': 'ts_code_p'})
+
+#合并两个基金表格
+#summary_fund_list = pd.concat([fund_list, fund_list_prior], axis=1, join='outer', sort=False)
+
+summary_fund_list = pd.merge(fund_list, fund_list_prior, on='symbol', how='outer')
+summary_fund_list = summary_fund_list.fillna(0)
+
+#计算两个基金表格，寻找差值
+summary_fund_list['stk_float_ratio_diff'] = summary_fund_list['stk_float_ratio'] - summary_fund_list['stk_float_ratio_p']
+summary_fund_list = summary_fund_list.sort_values(by='stk_float_ratio_diff', ascending=False)
+
+#剔除港股，增加申万分类
+summary_fund_list['exchange'] = summary_fund_list['symbol'].str[-2:]
+summary_fund_list = summary_fund_list[~summary_fund_list.symbol.str.contains('HK')]
+
+SW_index_list = fund_list = pd.read_excel (r'C:\Users\Austin\Desktop\Tushare\SW_index_list.xlsx', sheet_name='Constituent', index = False)
+SW_index_list = SW_index_list.loc[:, ['symbol','industry_name_lv1']]
+
+summary_fund_list2 = pd.merge(summary_fund_list, SW_index_list, on = 'symbol', how = 'left')
+
+#Export the df to excel
+summary_fund_list2.to_excel(r'C:\Users\Austin\Desktop\Tushare\summary_fund_list.xlsx', index = True)
+
+
