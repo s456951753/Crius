6e7c0fd2f00c050c508196792444590f3e44fc0c
Austin <austinyxh@hotmail.com>
2020-06-04 21:02:25 +1000

    error code in temp place

--- SectorScan/temp place.py
+++ SectorScan/temp place.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from rqalpha import run_code
+
 from rqalpha.api import *
+
+
+import numpy as np
+import math
+import Utils.numeric_utils as TuRq
 from datetime import date, datetime, timedelta
 import time as t
 import pandas as pd
@@ -16,11 +22,13 @@ from rqalpha.mod.rqalpha_mod_sys_accounts.api.api_stock import order_target_valu
 import Utils.configuration_file_service as config_service
 import tushare as ts
 
+import logging
+
+import builtins
 token = config_service.getProperty(section_name=config_service.TOKEN_SECTION_NAME,
                                    property_name=config_service.TS_TOKEN_NAME)
 pro = ts.pro_api(token)
 
-
-df = ts.pro_bar(ts_code='000001.SZ', adj='hfq', start_date='20180101', end_date='20181011')
+df = pro.adj_factor(ts_code='000001.SZ', trade_date='')
 
 print(df)
