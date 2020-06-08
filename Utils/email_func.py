2b9ea61ba8aaee3062a3a1dc66fe3dad9ccacdf3
Austin <austinyxh@hotmail.com>
2020-06-08 21:28:56 +1000

    AY email function

---
+++ Utils/email_func.py
@@ -0,0 +1,25 @@
+import smtplib, ssl
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "austin.yun365@gmail.com"  # Enter your address
+
+receiver_email_1 = "austinyxh@hotmail.com"
+receiver_email_2 = "ee07b238@gmail.com"
+
+password = "19880729Yxh!" #("Type your password and press enter: ")
+
+message = """\
+Subject: Hi there
+
+This message is sent from Python."""
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email_1, message)
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email_2, message)
