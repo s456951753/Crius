a7e04131fe68c97ac43881c9ae7d1317cef50385
Austin <austinyxh@hotmail.com>
2020-06-24 23:17:37 +1000

    SQL

---
+++ MySql/stock_basic.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from rqalpha.api import *
+
+import Utils.numeric_utils as TuRq
+from datetime import datetime, timedelta
+from datetime import date
+import time as t
+import pandas as pd
+
+#Load Tushare
+from rqalpha.apis.api_base import history_bars, get_position
+from rqalpha.mod.rqalpha_mod_sys_accounts.api.api_stock import order_target_value, order_value
+
+import Utils.configuration_file_service as config_service
+import tushare as ts
+
+token = config_service.getProperty(section_name=config_service.TOKEN_SECTION_NAME,
+                                   property_name=config_service.TS_TOKEN_NAME)
+pro = ts.pro_api(token)
+
+# coding=utf-8
+
+import pandas as pd
+import tushare as ts
+from sqlalchemy import create_engine
+
+from MySql.mysql_tables_structure import Base
+import Utils.mysql_functions as mf
+
+# 创建数据库引擎
+engine = create_engine('mysql://root:3c311a@127.0.0.1/Crius_SQL?charset=utf8mb4')
+conn = engine.connect()
+
+# 创建mysql所有表结构
+Base.metadata.create_all(engine)
+
+# 股票列表
+mf.update_stock_basic(engine, pro, 3, 2)
+
---
+++ Utils/mysql_functions.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+#建立获取tushare数据函数，存为tushare_data.py
+
+# coding=utf-8
+
+import time
+import pandas as pd
+
+
+def get_stock_basic(pro, retry_count=3, pause=2):
+    """股票列表 数据"""
+    frame = []
+    for status in ['L', 'D', 'P']:
+        for _ in range(retry_count):
+            try:
+                df = pro.stock_basic(exchange='', list_status=status,
+                                     fields='ts_code,symbol,name,area,industry,fullname,enname,market, \
+                                    exchange,curr_type,list_status,list_date,delist_date,is_hs')
+            except:
+                time.sleep(pause)
+            else:
+                frame.append(df)
+                break
+
+    return frame
+
+
+#更新“股票列表”数据，存为mysql_functions.py
+
+import pandas as pd
+import tushare as ts
+
+def truncate_update(engine, data, table_name):
+    """删除mysql表所有数据，to_sql追加新数据"""
+    conn = engine.connect()
+    conn.execute('truncate ' + table_name)
+    data.to_sql(table_name, engine, if_exists='append', index=False)
+
+
+def update_stock_basic(engine, pro, retry_count, pause):
+    """更新 股票信息 所有数据"""
+    data = get_stock_basic(pro, retry_count, pause)
+    truncate_update(engine, data, 'stock_basic')
+
