2ca03545cea1bf665bc83cbd53dd200e71fedecb
Austin <austinyxh@hotmail.com>
2020-08-30 22:35:24 +1000

    AY

--- MySql/daily_sharded.py
+++ MySql/daily_sharded.py
@@ -164,4 +164,6 @@ metadata.create_all(engine)
 
 # codes = get_ts_code(engine)
 # update_bulk_daily_using_code_by_year(engine, pro, codes, '19901219', datetime.date.today().strftime("%Y%m%d"), 3, 1)
-update_bulk_daily_by_day(pro=pro, start_date='19901219', end_date=datetime.date.today().strftime("%Y%m%d"))
+#update_bulk_daily_by_day(engine, pro=pro, start_date='19901219', end_date=datetime.date.today().strftime("%Y%m%d"))
+update_bulk_daily_by_day(engine, pro=pro, start_date='19990501', end_date='20200830')
+
