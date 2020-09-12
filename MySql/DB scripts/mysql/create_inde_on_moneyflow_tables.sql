86aaaa8c27d3c7d803c36ef9f4ae00d55d0b383a
Yuan Liu <ee07b238@gmail.com>
2020-09-12 17:41:06 +1200

    scripts to create index on tables

---
+++ MySql/DB scripts/mysql/create_inde_on_moneyflow_tables.sql
@@ -0,0 +1,30 @@
+create unique index unique_moneyflow_1990_1994_tscode_date on moneyflow_1990_1994 (ts_code,trade_date);
+create index ind_moneyflow_1990_1994_tscode on moneyflow_1990_1994(ts_code);
+create index ind_moneyflow_1990_1994_trade_date on moneyflow_1990_1994(trade_date);
+
+create unique index unique_moneyflow_1995_1999_tscode_date on moneyflow_1995_1999 (ts_code,trade_date);
+create index ind_moneyflow_1995_1999_tscode on moneyflow_1995_1999(ts_code);
+create index ind_moneyflow_1995_1999_trade_date on moneyflow_1995_1999(trade_date);
+
+create unique index unique_moneyflow_2000_2004_tscode_date on moneyflow_2000_2004 (ts_code,trade_date);
+create index ind_moneyflow_2000_2004_tscode on moneyflow_2000_2004(ts_code);
+create index ind_moneyflow_2000_2004_trade_date on moneyflow_2000_2004(trade_date);
+
+create unique index unique_moneyflow_2005_2009_tscode_date on moneyflow_2005_2009 (ts_code,trade_date);
+create index ind_moneyflow_2005_2009_tscode on moneyflow_2005_2009(ts_code);
+create index ind_moneyflow_2005_2009_trade_date on moneyflow_2005_2009(trade_date);
+
+
+create unique index unique_moneyflow_2010_2014_tscode_date on moneyflow_2010_2014 (ts_code,trade_date);
+create index ind_moneyflow_2010_2014_tscode on moneyflow_2010_2014(ts_code);
+create index ind_moneyflow_2010_2014_trade_date on moneyflow_2010_2014(trade_date);
+
+
+create unique index unique_moneyflow_2015_2019_tscode_date on moneyflow_2015_2019 (ts_code,trade_date);
+create index ind_moneyflow_2015_2019_tscode on moneyflow_2015_2019(ts_code);
+create index ind_moneyflow_2015_2019_trade_date on moneyflow_2015_2019(trade_date);
+
+
+create unique index unique_moneyflow_2020_2024_tscode_date on moneyflow_2020_2024 (ts_code,trade_date);
+create index ind_moneyflow_2020_2024_tscode on moneyflow_2020_2024(ts_code);
+create index ind_moneyflow_2020_2024_trade_date on moneyflow_2020_2024(trade_date);
