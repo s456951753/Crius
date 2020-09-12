b4493278a2675aab2851d69c7103b558df55202c
Yuan Liu <ee07b238@gmail.com>
2020-09-12 19:51:11 +1200

    scripts to add an int column for trade date to better support range scan.

--- MySql/DB scripts/mysql/create_inde_on_adj_factor_tables.sql
+++ MySql/DB scripts/mysql/create_inde_on_adj_factor_tables.sql
@@ -28,3 +28,32 @@ create index ind_adj_factor_2015_2019_trade_date on adj_factor_2015_2019(trade_d
 create unique index unique_adj_factor_2020_2024_tscode_date on adj_factor_2020_2024 (ts_code,trade_date);
 create index ind_adj_factor_2020_2024_tscode on adj_factor_2020_2024(ts_code);
 create index ind_adj_factor_2020_2024_trade_date on adj_factor_2020_2024(trade_date);
+
+alter table adj_factor_1990_1994 add column trade_date_int int;
+update adj_factor_1990_1994 set trade_date_int = cast(trade_date as unsigned);
+create index ind_adj_factor_1990_1994_trade_date_int on adj_factor_1990_1994(trade_date_int);
+
+alter table adj_factor_1995_1999 add column trade_date_int int;
+update adj_factor_1995_1999 set trade_date_int = cast(trade_date as unsigned);
+create index ind_adj_factor_1995_1999_trade_date_int on adj_factor_1995_1999(trade_date_int);
+
+alter table adj_factor_2000_2004 add column trade_date_int int;
+update adj_factor_2000_2004 set trade_date_int = cast(trade_date as unsigned);
+create index ind_adj_factor_2000_2004_trade_date_int on adj_factor_2000_2004(trade_date_int);
+
+alter table adj_factor_2005_2009 add column trade_date_int int;
+update adj_factor_2005_2009 set trade_date_int = cast(trade_date as unsigned);
+create index ind_adj_factor_2005_2009_trade_date_int on adj_factor_2005_2009(trade_date_int);
+
+alter table adj_factor_2010_2014 add column trade_date_int int;
+update adj_factor_2010_2014 set trade_date_int = cast(trade_date as unsigned);
+create index ind_adj_factor_2010_2014_trade_date_int on adj_factor_2010_2014(trade_date_int);
+
+alter table adj_factor_2015_2019 add column trade_date_int int;
+update adj_factor_2015_2019 set trade_date_int = cast(trade_date as unsigned);
+create index ind_adj_factor_2015_2019_trade_date_int on adj_factor_2015_2019(trade_date_int);
+
+alter table adj_factor_2020_2024 add column trade_date_int int;
+update adj_factor_2020_2024 set trade_date_int = cast(trade_date as unsigned);
+create index ind_adj_factor_2020_2024_trade_date_int on adj_factor_2020_2024(trade_date_int);
+
